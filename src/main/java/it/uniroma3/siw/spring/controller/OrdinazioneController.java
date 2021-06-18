e28e66812b81e9e17821a94293e8915805158d0b
StormyT95 <63358109+StormyT95@users.noreply.github.com>
2021-06-18 16:11:44 +0200

    Delete src directory

--- src/main/java/it/uniroma3/siw/spring/controller/OrdinazioneController.java
+++
@@ -1,5 +0,0 @@
-package it.uniroma3.siw.spring.controller;
-
-public class OrdinazioneController {
-
-}
--- src/main/java/it/uniroma3/siw/spring/repository/OrdinazioneRepository.java
+++
@@ -1,9 +0,0 @@
-package it.uniroma3.siw.spring.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import it.uniroma3.siw.spring.model.Ordinazione;
-
-public interface OrdinazioneRepository extends CrudRepository<Ordinazione, Long> {
-		
-}
